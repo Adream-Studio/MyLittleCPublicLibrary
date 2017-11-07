e89cf2cc86619aef7e47599087edc74850779b3a
Joe Nahm <joenahm@yeah.net>
2017-11-07 18:57:30 +0800

    Add binary-tree From WIN10

--- Makefile
+++ Makefile
@@ -1,18 +1,21 @@
-objects = test.o stack.o sllist.o sqlist.o
+objects = test.o stack.o sllist.o sqlist.o bt.o
 test : $(objects)
 	gcc -o test $(objects)
 
 test.o : test.c ./stack/stack.h ./link-list/sllist.h
-	gcc -c test.c
+	gcc -c -g test.c
 
-stack.o : ./stack/stack.c ./stack/stack.h ./stack/stack_elem_type.h ./stack/stack_storage.h
-	gcc -c ./stack/stack.c
-
-sllist.o : ./link-list/sllist.c ./link-list/sllist.h ./link-list/sllist_elem_type.h
-	gcc -c ./link-list/sllist.c
+bt.o : ./binary-tree/bt.c ./binary-tree/bt.h ./binary-tree/bt_elem_type.h
+	gcc -c -g ./binary-tree/bt.c
 
 sqlist.o : ./sequence-list/sqlist.c ./sequence-list/sqlist.h ./sequence-list/sqlist_elem_type.h
-	gcc -c ./sequence-list/sqlist.c
+	gcc -c -g ./sequence-list/sqlist.c
+
+stack.o : ./stack/stack.c ./stack/stack.h ./stack/stack_elem_type.h ./stack/stack_storage.h
+	gcc -c -g ./stack/stack.c
+
+sllist.o : ./link-list/sllist.c ./link-list/sllist.h ./link-list/sllist_elem_type.h
+	gcc -c -g ./link-list/sllist.c
 
 clean-all : 
 	rm test $(objects)
---
+++ binary-tree/bt.c
@@ -0,0 +1,108 @@
+/* 
+	Author	:	Joe Nahm
+	Email	:	joenahm@yeah.net
+ */
+#include "bt.h"
+
+/* private functions */
+int isNull(bt_elem_type value){
+	int status;
+	if( value.isNull ){
+		status = TRUE;
+	}else{
+		status = FALSE;
+	}
+
+	return status;
+}
+
+void freeNode(BTNode *node){
+	//printf("free the node of value: %d\n", getData(node->value));
+	free(node);
+}
+/* private functions */
+
+/* public functions */
+void getInput(bt_elem_type *buffer){
+	int temp = getchar();
+	if( temp != ' ' ){
+		buffer->data = temp - '0';
+		buffer->isNull = FALSE;
+	}else{
+		buffer->isNull = TRUE;
+	}
+}
+
+int getData(bt_elem_type value){
+	return value.data;
+}
+
+void visit(BTNode *nodep){
+	printf("%s %d\n", bt_isLeaf(*nodep)?"Leaf:":"Branch:", getData(nodep->value));
+}
+
+void bt_init(BT *rootpp){
+	*rootpp = NULL;
+}
+
+void bt_create(BT *treeNode){
+	bt_elem_type buffer;
+	getInput(&buffer);
+
+	if( isNull(buffer) ){
+		*treeNode = NULL;
+	}else{
+		*treeNode = (BTNode*)malloc(sizeof(BTNode));
+		if( *treeNode != NULL ){
+			(*treeNode)->value = buffer;
+
+			bt_create(&(*treeNode)->lChild);
+			bt_create(&(*treeNode)->rChild);
+		}else{
+			fprintf(stderr, "ERROR(bt_create): failed to create binary tree node !\n");
+			exit(EXIT_FAILURE);
+		}	
+	}
+}
+
+int bt_isLeaf(BTNode node){
+	int status;
+	if( node.lChild == NULL
+		&& node.rChild == NULL ){
+
+		status = TRUE;
+	}else{
+		status =FALSE;
+	}
+
+	return status;
+}
+
+void bt_preOrderTraverse(BT treeNode, void (*visit)(BTNode *node)){
+	if( treeNode != NULL ){
+		(*visit)(treeNode);
+		bt_preOrderTraverse(treeNode->lChild,visit);
+		bt_preOrderTraverse(treeNode->rChild,visit);
+	}
+}
+
+void bt_inOrderTraverse(BT treeNode, void (*visit)(BTNode *node)){
+	if( treeNode != NULL ){
+		bt_inOrderTraverse(treeNode->lChild,visit);
+		(*visit)(treeNode);
+		bt_inOrderTraverse(treeNode->rChild,visit);
+	}
+}
+
+void bt_postOrderTraverse(BT treeNode, void(*visit)(BTNode *nodep)){
+	if( treeNode != NULL ){
+		bt_inOrderTraverse(treeNode->lChild,visit);
+		bt_inOrderTraverse(treeNode->rChild,visit);
+		(*visit)(treeNode);
+	}
+}
+
+void bt_destroy(BT tree){
+	bt_postOrderTraverse(tree,&freeNode);
+}
+/* public functions */
