ced675fd086be971d50467d439e2e35eb13a2fd8
Joe Nahm <joenahm@yeah.net>
2017-11-20 17:50:56 +0800

    Update From 303

--- digraph/dg_elem_type.h
+++ digraph/dg_elem_type.h
@@ -5,10 +5,25 @@
 #ifndef _DG_ELEM_TYPE_
 #define _DG_ELEM_TYPE_
 
+#define MAX_VERT_NUM 100
+
 typedef struct{
 	int data;
 	int isNull;
 } dg_elem_type;
 
+typedef struct EDGENODE{
+	int adjvex;
+	struct EDGENODE *next;
+} EdgeNode;
+
+typedef struct VERTNODE{
+	dg_elem_type value;
+	EdgeNode *firstEdge;
+} VertNode, AdjList[MAX_VERT_NUM];
+
+typedef struct{
+	
+}
 
 #endif
