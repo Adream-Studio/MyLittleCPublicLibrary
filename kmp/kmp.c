c285b0209fc3bd9922f61c6e5bc33579df628de5
Joe Nahm -WIN10 <joenahm@yeah.net>
2017-12-03 23:45:05 +0800

    update algorithm kmp From WIN10

--- kmp/kmp.c
+++ kmp/kmp.c
@@ -1,37 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void getPrefix(char const *str, int *arr, int length){
-	arr[0] = -1;
-
-	int i;
-	for( i = 1 ; i < length ; i++ ){
-		int preVal = 0;
-		int j = 0;
-		while( j < i 
-			&& j != (i-j)
-			&& str[j]==str[i-j] ){
-
-			preVal++;
-			j++;
-		}
-
-		arr[i] = preVal;
+void getPrefix(char const *patternStr, int *prefixArr, int length){
+	prefixArr[0] = -1;
+	int i = 0;
+	int j = -1;
+	while( i < length-1 ){
+		if ( j == -1 || patternStr[i] == patternStr[j] ){
+			i++;
+            j++;
+         	prefixArr[i] = j;
+         }else{
+            j = prefixArr[j];
+        }
 	}
+}
+
+int kmp(char const *str, char const *patternStr){
+	int sLen = strlen(str);
+	int pLen = strlen(patternStr);
+	int *prefixArr = (int*)malloc(sizeof(int)*pLen);
+	getPrefix(patternStr,prefixArr,pLen);
+
+	int i =0, j = 0;
+	while ( i < sLen && j < pLen ){  
+        if ( j == -1 || str[i] == patternStr[j] ){  
+        	i++;  
+        	j++;  
+        }else{
+        	j = prefixArr[j];  
+        }  
+    }
+	free(prefixArr);
+
+    int index;  
+    if ( j == pLen ){
+    	index = i-j; 
+    }else{
+        index = -1;
+    }
+	return index;
 }
 
 int main(void){
 	char *str1 = "abaacababcac";
 	char *str2 = "ababc";
-	int arr[6];
-
-	//-1 0 0 0 0 0
-
-	getPrefix(str2,arr,5);
-	int i;
-	for( i = 0 ; i < 5 ; i++ ){
-		printf(" %d", arr[i]);
-	}
+	int index = kmp(str1,str2);
+	printf("%d",index);
 
 	return 0;
 }
